430664b27ca1ecba0659796a5f3275079b2312df
Aleksej Zelezniak <azelnet@gmail.com>
2017-02-21 14:29:36 +0100

    Fixes bug in pipeline_test.sh

--- pipeline_test.sh
+++ pipeline_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./copy_data.sh
+#./copy_data.sh
 logs="./logs"
 mkdir -p $logs
 
@@ -25,17 +25,17 @@ wait_job2=`bsub -J "comet[1-$fLen]%30" < ./comet_array.sh | sed 's/[^0-9]*//g'`
 
 
 #tandem2pepxml
-#wait_job2=`bsub -J "comet[1-$fLen]%30" < ./comet_array.sh | sed 's/[^0-9]*//g'`
+wait_job3=`bsub -J "comet[1-$fLen]%30" < ./tandem2pepxml.sh | sed 's/[^0-9]*//g'`
 
 #run PeptideProphet
-wait_job3=`bsub -J xinteract -w "ended($wait_job1)&&ended($wait_job2)" < ./xinteract.sh | sed 's/[^0-9]*//g'`
-echo "Job PeptidePropthet, $wait_job3 has been submitted"
+wait_job4=`bsub -J xinteract -w "ended($wait_job1)&&ended($wait_job2)&&ended($wait_job3)" < ./xinteract.sh | sed 's/[^0-9]*//g'`
+echo "Job PeptidePropthet, $wait_job4 has been submitted"
 
 #iprophet
-wait_job4=`bsub -J interprophet -w "ended($wait_job3)" < ./interprophet.sh | sed 's/[^0-9]*//g'`
-echo "Job iProphet, $wait_job4 has been submitted"
+wait_job5=`bsub -J interprophet -w "ended($wait_job4)" < ./interprophet.sh | sed 's/[^0-9]*//g'`
+echo "Job iProphet, $wait_job5 has been submitted"
 
 #mayu and spectrast with certain ipropet probabylity for given fdr(default: 0,01)
-wait_job5=`bsub -J spectrast -w "ended($wait_job4)" < ./spectrast.sh | sed 's/[^0-9]*//g'`
-echo "Jobs Mayu and spectrast, $wait_job5 has been submitted"
+wait_job6=`bsub -J spectrast -w "ended($wait_job5)" < ./spectrast.sh | sed 's/[^0-9]*//g'`
+echo "Jobs Mayu and spectrast, $wait_job6 has been submitted"
 
